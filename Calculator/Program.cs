5043f8d40e9d961f3658398e217d1e4501d254d7
Timasuper05 <76902286+Timasuper05@users.noreply.github.com>
2021-09-02 18:07:04 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Numbers();
+            }
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так");
+            }
+
+            Console.WriteLine("Снова? Y/N Да или Нет");
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("\n");
+                Numbers();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+            
+
+        }
+        public static void Numbers()
+        {
+            Console.WriteLine("Введите первое число:");
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действие:");
+            char action = char.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double number2 = double.Parse(Console.ReadLine());
+            double result = 0;
+            switch (action)
+            {
+                case '+':
+                    result = number + number2;
+                    Console.WriteLine($"Результат: {result}");
+                    break;
+                case '-':
+                    result = number - number2;
+                    Console.WriteLine($"Результат: {result}");
+                    break;
+                case '*':
+                    result = number * number2;
+                    Console.WriteLine($"Результат: {result}");
+                    break;
+                case '/':
+                    if (number2 != 0)
+                    {
+                        result = number / number2;
+                        Console.WriteLine($"Результат: {result}");
+                        break;
+                    }
+                    else
+                    {
+                        result = 0;
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Неизвестное действие");
+                    break;
+
+            }
+            
+        }
+    }
+}
